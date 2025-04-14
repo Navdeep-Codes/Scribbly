483b5bd3d754f6e8da4f2d6f563e761ba7b9132a
S.Navdeep Singh <navdeep13dps@gmail.com>
2025-04-14 15:09:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware to serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Route to serve homepage
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/dashboard.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Notive is running at http://localhost:${PORT}`);
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/notes.html'));
+});
+
+app.get('/planner', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/planner.html'));
+});
+
+app.get('/habits', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/habits.html'));
+});
+
+app.get('/math', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/math.html'));
 });
